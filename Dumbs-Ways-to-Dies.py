41ac7e8eec10b6c688590ad9cd5b0bc37ef0e68e
jeansevenz <spinolaseven@gmail.com>
2021-11-19 17:28:15 -0300

    Iniciando commit

---
+++ Dumbs-Ways-to-Dies.py
@@ -0,0 +1,121 @@
+def questaoA_1():
+    step = input("Aos 75 minutos do segundo tempo, o jogo estava em seu clímax, quando o time espanhol estava imprimindo um jogo muito violento, com faltas táticas e pressionando o juiz em qualquer lance que não fosse a seu favor, até que o jogador Iniesta entrou em um lance duro contra o atacante Gabigol. Quando o árbitro Alisson puxou o cartão vermelho, um crime aconteceu\n\n (aperte n para continuar)")
+    
+    
+    pergunta1 = input("\n\nRESOLVA O CASO:\n Qual foi o crime?\n\na) Um touro invadiu o campo e atacou o juiz com seus chifres\nb) O juiz foi atingido com fogos de artifícios. \nc) Iniesta agrediu o juiz com um chute em sua cabeça.\n\nDigite a, b ou c: ")
+    
+    if pergunta1== "a":
+        print("Parabens, " + nome + "! Você resolveu o caso! Temos um xerok roumes! ")
+    elif pergunta1== 'b':
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+    elif pergunta1== 'c':
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+
+    tryagain = input('Deseja jogar novamente? S/N ')
+    if tryagain== 'S':
+        jogo()
+        
+def questaoA_2():
+    step = input("O jogo estava muito equilibrado, ambos os times não abriam mão de atacar. O Barcelona tinha a seu favor o fator casa, e o Flamengo, sua apaixonada torcida que cantava incessantemente, mas, os torcedores rubro-negros estranharam a ausência de sua mascote. Quando, no intervalo, um tweet viraliza com a acusação de que um jogador do Barcelona invadiu o CT em que o Flamengo estava treinando, e roubou sua mascote, inclusive com um vídeo das câmeras de segurança. E em retaliação, a torcida resolve dar o troco.\n\n (aperte n para continuar)")
+    
+    
+    pergunta1 = input("\n\nRESOLVA O CASO:\nO que foi feito?\n\na) Um torcedor invade o campo e agride o Iniesta, com um mata leão.\nb) O Urubu é encontrado, solto e ataca a jugular do meia do Barcelona. \nc) A torcida chama Iniesta de latino, ele se ofende e morre de tanto chorar\n\nDigite a, b ou c: ")
+    
+    if pergunta1== "a":
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+    elif pergunta1== 'b':
+        print("Parabens, " + nome + "! Você resolveu o caso! Temos um xerok roumes! ")
+    elif pergunta1== 'c':
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+
+    tryagain = input('Deseja jogar novamente? S/N ')
+    if tryagain== 'S':
+        jogo()
+        
+def questaoA():
+    step = input("Realmente, o que ele viu naquele gramado o deixou chocado. A partida foi interrompida, e Rodinei começou a investigar e descobriu os seguintes fatos:\n\n (aperte n para continuar)")
+    step = input("Na noite anterior ao jogo, o juiz [Alisson] ficou até tarde da noite em reunião, se preparando para sua grande atuação do dia seguinte. Ao chegar em casa na madrugada, ele teve uma grande surpresa: sua esposa estava traindo-o. Ele não conseguiu ver quem era o homem que esteve com ela, mas sabia que era um jogador, pois o mesmo esqueceu um dos meiões ao lado da cama.\n\n (aperte n para continuar)")
+    step = input("No dia do jogo, pela manhã, o jogador Iniesta, do Barcelona, invadiu secretamente o centro de treinamento onde o time do Flamengo estava realizando a preparação final para o jogo e roubou a mascote que estava sob responsabilidade do Sr Denir, o massagista.\n\n (aperte n para continuar)")
+    step = input("Uma semana antes do jogo, as torcidas organizadas do Flamengo e do Barcelona estavam planejando um confronto entre torcidas, como se fosse para marcar território. A polícia civil, acompanhou de perto o fato, mas sem interferir diretamente e utilizou as câmeras de segurança, mas identificar os torcedores. \n\n (aperte n para continuar)")
+
+    
+    
+    q2 = input("\n\nQuem você acha que foi a vítima da tragédia?\n\na) o arbitro Alisson\nb) o meia Iniesta \n\nDigite a ou b: ")
+    
+    if q2== "a":
+        questaoA_1()
+    elif q2== 'b':
+        questaoA_2()
+    
+def questaoB_1():
+    step = input("Aos 75 minutos do segundo tempo, o jogo estava em seu clímax, quando o time espanhol estava imprimindo um jogo muito violento, com faltas táticas e pressionando o juiz em qualquer lance que não fosse a seu favor, até que o jogador Iniesta entrou em um lance duro contra o atacante Gabigol. Enquanto isso, as torcidas ficaram exaltadas e começaram a atirar de todas as partes da arquibancada, copos e latas de bebidas em direção ao campo. Planejando precisavam esfriar a cabeça dos torcedores, a polícia interveio. De repente um torcedor morreu.\n\n (aperte n para continuar)")
+    
+    
+    pergunta1 = input("\n\nRESOLVA O CASO:\n Qual foi a causa da morte?\n\na) Bebeu cerveja quente e morreu com dor de barriga\nb) Ataque cardíaco vendo um jogador perder o gol debaixo da trave. \nc) Foi atingido com um copo de urina na cabeça, escorreu para os olhos, tropeçou e caiu escada a baixo.\n\nDigite a, b ou c: ")
+    
+    if pergunta1== "a":
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+    elif pergunta1== 'b':
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+    elif pergunta1== 'c':
+        print("Parabens, " + nome + "! Você resolveu o caso! Temos um xerok roumes! ")
+
+    tryagain = input('Deseja jogar novamente? S/N ')
+    if tryagain== 'S':
+        jogo()
+        
+def questaoB_2():
+    step = input("No mês de Julho, no auge do verão europeu, os ambulantes costumam faturar o triplo em comparação ao inverno, com a venda de bebidas. Era sábado e a cidade da Catalunha estava movimentada pois haveria um clássico Global entre Barcelona x Flamengo. Dois dos times mais poderosos de seus países. O senhor Javier era um motorista que complementava sua renda trabalhando como ambulante aos fins de semana, nos estádios. No dia do jogo, fez toda a preparação para começar os trabalhos durante o jogo. O clima estava quente tanto dentro de campo quanto fora dele.\n\n (aperte n para continuar)")
+    
+    
+    pergunta1 = input("\n\nRESOLVA O CASO:\nO que aconteceu com o senhor Javier?\n\na) Foi agredido porque as bebidas estavam quentes.\nb) Ganhou 100 euros de gorjeta e teve uma parada cardíaca \nc) Descobriu que seria pai aos 89 anos e infartou\n\nDigite a, b ou c: ")
+    
+    if pergunta1== "a":
+        print("Parabens, " + nome + "! Você resolveu o caso! Temos um xerok roumes! ")
+    elif pergunta1== 'b':
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+    elif pergunta1== 'c':
+        print('Como dizia o Faustão: Errooooou! :( tente novamente! ')
+
+    tryagain = input('Deseja jogar novamente? S/N ')
+    if tryagain== 'S':
+        jogo()
+        
+def questaoB():
+    step = input("O jogo estava eletrizante, as torcidas gritavam e cantavam à plenos pulmões, nunca se viu um jogo tão disputado assim em toda a Espanha\n\n (aperte n para continuar)")
+    step = input("Os torcedores mal piscavam, pois não queriam perder nenhum segundo do jogo, nem mesmo para ir ao banheiro. Enquanto isso, os vendedores ambulantes vendiam todas as suas bebidas à preços exorbitantes.\n\n (aperte n para continuar)")
+    
+    
+    q2 = input("\n\nQuem foi a vítima da tragédia?\n\na) um torcedor\nb) o ambulante \n\nDigite a ou b: ")
+    
+    if q2== "a":
+        questaoB_1()
+    elif q2== 'b':
+        questaoB_2()
+    
+
+
+
+def jogo():
+    print("Olá! Bem-vindo(a) ao Dumbs Ways to Die!")
+    nome = input("Para começar, digite seu nome: ")
+    
+    print("\nBeleza, " + nome + "! Vamos começar? \nNossa história começa assim:\n\n ")
+    
+    step = input("Rodinei, um investigador fanático por futebol. Ao acordar naquela manhã, ele teve um pressentimento de que algo grande iria acontecer naquele jogo, então foi ao estádio preparado para qualquer coisa. \n Era um grande dia, pois dois grandes clubes iam se enfrentar na Espanha: Flamengo x Barcelona.\n\n (aperte n para continuar)")
+    
+    
+    
+    q1 = input("\n\nRodinei, infelizmente, estava certo. Ele se deparou com uma tragédia naquele estádio. Onde você acha que aconteceu o fato?\n\na) A tragédia aconteceu em campo \nb) A tragédia aconteceu na arquibancada \n\nDigite a ou b: ")
+    
+    if q1== "a":
+        questaoA()
+    elif q1== 'b':
+        questaoB()
+    
+    
+    
+    
+    
+
+jogo()
